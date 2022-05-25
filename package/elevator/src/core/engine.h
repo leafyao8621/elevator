a04c444eca594432638d493af6665aace57d7be1
Yi Yao <leaf_yao8621@hotmail.com>
2022-05-25 14:21:27 -0700

    Updates

--- package/elevator/src/core/engine.h
+++ package/elevator/src/core/engine.h
@@ -4,10 +4,13 @@
 #include "model.h"
 #include "statistics.h"
 #include "../util/priority_queue.h"
+#include "../util/mt19937.h"
 
 struct Engine {
     uint64_t num_iter, id;
     double time_now;
+    struct MT19937 gen;
+    double *rates;
     struct Model model;
     struct Statistics statistics;
     struct PriorityQueue priority_queue;
